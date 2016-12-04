de9883853c53be5adb8980efcea3184fb736fb1c
Quinten Pohl <quinten.pohl@gmail.com>
2016-12-04 20:06:07 +0000

    Repo Setup

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+    public GameObject Platform;
+    public Transform GenerationPoint;
+    public float DistanceBetween;
+
+    private float platformWidth;
+
+	void Start () {
+        platformWidth = Platform.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+
+	void Update () {
+		if(transform.position.x < GenerationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth + DistanceBetween, transform.position.y, transform.position.z);
+        }
+
+        Instantiate(Platform, transform.position, transform.rotation);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float MoveSpeed;
+    public float JumpForce;
+    public bool grounded;
+    public LayerMask groundLayer;
+
+    private Animator playerAnimator;
+    private Rigidbody2D playerRigidBody;
+    private Collider2D playerCollider;
+
+	void Start () {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        playerAnimator = GetComponent<Animator>();
+    }
+	
+	void Update () {
+        grounded = Physics2D.IsTouchingLayers(playerCollider, groundLayer);
+
+        playerRigidBody.velocity = new Vector2(MoveSpeed, playerRigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, JumpForce);
+        }
+
+        playerAnimator.SetBool("Grounded", grounded);
+        playerAnimator.SetFloat("Speed", playerRigidBody.velocity.x);
+	}
+}
