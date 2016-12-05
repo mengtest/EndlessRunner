26c6d4b49d58e5cd2fb140d1fdc00ebf4c058493
Quinten Pohl <Quinten_pohl@dell.com>
2016-12-05 16:55:01 +0000

    Added more platfroms + spawning random platforms

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 public class PlatformGenerator : MonoBehaviour {
 
     public GameObject Platform;
+    public GameObject[] Platforms;
     public Transform GenerationPoint;
     public ObjectPooler ObjectPool;
 
@@ -11,24 +13,35 @@ public class PlatformGenerator : MonoBehaviour {
     public float distanceBetweenMax;
     
     private float platformWidth;
-    
+    private int platformSelector;
+    private float[] plaformWidths;
+
+    [UsedImplicitly]
     void Start () {
-        platformWidth = Platform.GetComponent<BoxCollider2D>().size.x;
-	}
+        plaformWidths = new float[Platforms.Length];
+
+        for (int i = 0; i < Platforms.Length; i++)
+        {
+            plaformWidths[i] = Platforms[i].GetComponent<BoxCollider2D>().size.x;
+        }
+    }
 	
+    [UsedImplicitly]
 	void Update () {
 		if(transform.position.x < GenerationPoint.position.x)
         {
             DistanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+            platformSelector = Random.Range(0, Platforms.Length);
 
-            transform.position = new Vector3(transform.position.x + platformWidth + DistanceBetween, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x + plaformWidths[platformSelector] + DistanceBetween, transform.position.y, transform.position.z);
 
-            var newPlatform = ObjectPool.GetPooledObject();
+            Instantiate(Platforms[platformSelector], transform.position, transform.rotation);
+            //var newPlatform = ObjectPool.GetPooledObject();
 
-            newPlatform.transform.position = transform.position;
-            newPlatform.transform.rotation = transform.rotation;
+            //newPlatform.transform.position = transform.position;
+            //newPlatform.transform.rotation = transform.rotation;
 
-            newPlatform.SetActive(true);
+            //newPlatform.SetActive(true);
         }
     }
 }
